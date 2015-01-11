acceb34e0db29ed232915e24ff37394e5bd063db
Ahmed Sami <wenning9ahmed@hotmail.com>
2015-01-11 16:02:49 +0200

    More Angular for tweet stream

--- js/ajs-app.js
+++ js/ajs-app.js
@@ -15,13 +15,25 @@
 			user: 'Ahmed Sami Mohamed',
 			username: '@w9ahmed',
 			tweet: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-			time: '45s'
+			time: '45s',
+			retweets: 5,
+			favorites: 3
 		},
 		{
 			user: 'Ahmed Ezzat',
 			username: '@is7yX',
 			tweet: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-			time: '1m'
+			time: '1m',
+			retweets: 3,
+			favorites: 3
+		},
+		{
+			user: 'Ahmed Sami Mohamed',
+			username: '@w9ahmed',
+			tweet: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			time: '45s',
+			retweets: 1,
+			favorites: 1
 		}];
 	});
 
