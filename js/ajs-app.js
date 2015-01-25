c183c5f73c9117771a0b523311b15dd4e64a6300
Ahmed Sami <wenning9ahmed@hotmail.com>
2015-01-25 15:26:53 +0200

    Now, using AJAX requests to get data from JSON files

--- js/ajs-app.js
+++ js/ajs-app.js
@@ -45,35 +45,13 @@
 	});
 
 	// Tweets Stream Controller
-	app.controller('StreamCtrl', function() {
-		this.tweets = [
-		{
-			user: 'Ahmed Sami Mohamed',
-			username: '@w9ahmed',
-			tweet: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-			time: '45s',
-			retweets: 5,
-			favorites: 3,
-			self: true
-		},
-		{
-			user: 'Ahmed Ezzat',
-			username: '@is7yX',
-			tweet: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-			time: '1m',
-			retweets: 3,
-			favorites: 3,
-			self: false
-		},
-		{
-			user: 'Ahmed Sami Mohamed',
-			username: '@w9ahmed',
-			tweet: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-			time: '45s',
-			retweets: 1,
-			favorites: 1,
-			self: true
-		}];
+	app.controller('StreamCtrl', function($http) {
+		var parent = this;
+		this.tweets = [];
+		$http.get("../json/tweets.json")
+			.success(function(data, status) {
+				parent.tweets = data;
+			});
 	});
 
 	// Direct Messages Controller
