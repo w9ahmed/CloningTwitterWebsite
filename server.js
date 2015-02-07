1416f532b2075a0bd9721f8763510e6cff2732c2
Ahmed Sami <wenning9ahmed@hotmail.com>
2015-02-07 12:47:13 +0200

    Using Express instead of Connect

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-
-var site = 'CloningTwitterWebsite';
+var site = 'CloningTwitterWebsite'; // root folder name
+var port = 5000;
 
 console.log('Running Server for ' + site + '...');
 
-var port = 5000;
+var express = require('express');
+var app = express();
 
-connect().use(
-    serveStatic("../" + site)
-).listen(port);
+app.use(express.static('../CloningTwitterWebsite'));
+
+app.listen(port);
 
 console.log('Success...')
-console.log('Port: ' + port);
+console.log('Running server on port: ' + port);
+
+/* ******** ROUTING ******** */
+
+var home = express();
+var notifications = express();
+var discover = express();
+
+home.get('/', function (req, res) {
+  console.log(home.mountpath);
+  res.send('Home page');
+})
+
+notifications.get('/', function (req, res) {
+  console.log(notifications.mountpath);
+  res.send('Notifications page');
+})
+
+discover.get('/', function (req, res) {
+  console.log(discover.mountpath); // /admin
+  res.send('Discover page');
+})
+
+app.use('/home', home);
+app.use('/notifications', notifications);
+app.use('/discover', discover);
