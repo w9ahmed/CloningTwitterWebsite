9c8e0fd0ddbb12085aec70dd89fe3c00c5e759a7
Ahmed Sami <wenning9ahmed@hotmail.com>
2015-02-07 10:48:58 +0200

    Added some logs to server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
+var site = 'CloningTwitterWebsite';
+
+console.log('Running Server for ' + site + '...');
+
+var port = 5000;
+
 connect().use(
-    serveStatic("../CloningTwitterWebsite")
-).listen(5000);
+    serveStatic("../" + site)
+).listen(port);
+
+console.log('Success...')
+console.log('Port: ' + port);
