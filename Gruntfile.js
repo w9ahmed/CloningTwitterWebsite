6838e33325fd67a84d883982eb87f3078077bc40
Ahmed Sami <wenning9ahmed@hotmail.com>
2015-02-16 22:38:27 +0200

    some more grunt stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,17 +4,30 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
-      jsmin: {
+      build: {
         src: 'js/ajs-app.js',
         dest: 'js/min/ajs-app.min.js'
       }
+    },
+    cssmin: {
+    	build: {
+    		src: 'css/styles.css',
+    		dest: 'css/min/styles.min.css'
+    	}
+    },
+    watch: {
+    	files: ['css/styles.css', 'js/ajs-app.js'],
+    	tasks: ['uglify', 'cssmin']
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'cssmin']);
 
 };
