e21e34b81c748b90a7da132c2d55707601aaa34d
Ahmed Ezzat <AEzzatA@gmail.com>
2015-02-17 22:58:00 +0200

    install nodejs before npm. :)

---
+++ script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+clear
+
+
+if which nodejs >/dev/null; then
+    echo "nodejs exists"
+else
+    sudo apt-get install nodejs
+fi
+
+if which npm >/dev/null; then
+    echo "npm exists"
+else
+    sudo apt-get install npm
+fi
+
+echo 'Cloning Twitter Website script running...'
+echo
+
+echo 'Running npm install'
+npm install
+echo
+
+echo 'Running bower install'
+bower install
+echo
+
+echo 'Running grunt'
+grunt
+echo
+
+node server.js
