8ac559f6a4883c481c85781abddee353fc81c693
Ahmed Sami <asyn.nile@gmail.com>
2015-02-19 09:55:20 +0200

    Changes to server.js and .gitignore

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(express.static('../CloningTwitterWebsite'));
 app.listen(port);
 
 console.log('Running server on port: '.blue + port.toString().yellow);
-console.log('Success...'.green)
+console.log('Success...'.green );
+console.log('Go to (http://localhost:5000) on your browser!');
 
 /* ******** ROUTING ******** */
 
