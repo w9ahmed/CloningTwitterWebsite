6f4259ba913506ed8a2edfe9f59b3786d1311c06
Ahmed Sami <wenning9ahmed@hotmail.com>
2015-02-21 21:13:21 +0200

    Added Retweet and Favorite to tweet-box

--- components/tweet-box/tweet-box.js
+++ components/tweet-box/tweet-box.js
@@ -7,6 +7,24 @@ app.directive('tweetBox', function($templateCache) {
 		template: $templateCache.get('components/tweet-box/tweet-box'),
 		controller: function($scope) {
 
+			$scope.retweet = function() {
+				if($scope.tweet.retweeted) {
+					$scope.tweet.retweets--;
+				} else {
+					$scope.tweet.retweets++;
+				}
+				$scope.tweet.retweeted = !$scope.tweet.retweeted;
+			};
+
+			$scope.favorite = function() {
+				if($scope.tweet.favorited) {
+					$scope.tweet.favorites--;
+				} else {
+					$scope.tweet.favorites++;
+				}
+				$scope.tweet.favorited = !$scope.tweet.favorited;
+			};
+			
 		}
 	};
 });
