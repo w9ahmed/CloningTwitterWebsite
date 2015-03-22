8609a45f7f0832366c804ce07a9be3b6dcd8ce29
Ahmed Sami <wenning9ahmed@hotmail.com>
2015-03-22 22:10:35 +0200

    Reordering some lines

--- tests/direct-messages.js
+++ tests/direct-messages.js
@@ -1,32 +1,26 @@
 describe('Testing direct messages directive', function() {
 
-	var $compile, $rootScope, backend;
+	var $scope, $compile, $rootScope, backend, element;
 
 	var data = readJSON('tests/fixtures/messages.json');
 
 	beforeEach(module("twitter"));
 
-	beforeEach(inject(function(_$compile_, _$rootScope_) {
+	beforeEach(inject(function(_$compile_, _$rootScope_, $httpBackend) {
 		$compile = _$compile_;
 		$rootScope = _$rootScope_;
-	}));
+		backend = $httpBackend;
 
-	beforeEach(inject(function($httpBackend) {
 		$httpBackend.expectGET("/json/messages.json");
         $httpBackend.whenGET("/json/messages.json").respond(data);
+
+        element = $compile("<direct-messages></direct-messages>")($rootScope);
+
+        $rootScope.$digest();
 	}));
 
 	it('Replaces the element with the appropriate contents', function() {
-
-		var element = $compile("<direct-messages></direct-messages>")($rootScope);
-
-		$rootScope.$digest();
-
 		expect(element.html()).toContain('<div class="modal fade" id="directMessages" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">');
-	});
-
-	it('should behave...', function() {
-		// code...
 	});
 	
 });
